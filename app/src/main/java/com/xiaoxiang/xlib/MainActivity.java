28d36f0e92d61d174c5b000cb808ed762194dfcf
xiaoxiang <piner1029@gmail.com>
2021-07-26 18:03:19 +0800

    提交xspinner

--- app/src/main/java/com/xiaoxiang/xlib/MainActivity.java
+++ app/src/main/java/com/xiaoxiang/xlib/MainActivity.java
@@ -3,6 +3,10 @@ package com.xiaoxiang.xlib;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.xiaoxiang.xspinner.OnSpinnerSelectListener;
+import com.xiaoxiang.xspinner.XSpinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        XSpinner xSpinner = findViewById(R.id.xSpinner);
+        xSpinner.setOnSpinnerSelectListener(new OnSpinnerSelectListener() {
+            @Override
+            public void onSpinnerSelect(XSpinner spinner, int position) {
+                Toast.makeText(MainActivity.this, spinner.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
